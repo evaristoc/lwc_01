947adace61908e9b7ba1849ee671b31e8092b671
evaristoc <varikvi@yahoo.com>
2021-06-30 21:21:21 +0200

    first commit LWC example

--- force-app/main/default/lwc/accountFinder/accountFinder.js
+++ force-app/main/default/lwc/accountFinder/accountFinder.js
@@ -1,11 +1,27 @@
-import { LightningElement } from 'lwc';
+// import { LightningElement } from 'lwc';
+
+// export default class AccountFinder extends LightningElement {
+//     annualRevenue = null;
+//     handleChange(event) {
+//         this.annualRevenue = event.detail.value;
+//     }
+//     reset() {
+//         this.annualRevenue = null;
+//     }
+// }
+import { LightningElement, wire } from 'lwc';
+import queryAccountsByRevenue from '@salesforce/apex/AccountListControllerLwc.queryAccountsByRevenue';
+
 
 export default class AccountFinder extends LightningElement {
     annualRevenue = null;
+
     handleChange(event) {
         this.annualRevenue = event.detail.value;
     }
     reset() {
         this.annualRevenue = null;
     }
+
+    @wire(queryAccountsByRevenue, { annualRevenue: '$annualRevenue' }) accounts;
 }
